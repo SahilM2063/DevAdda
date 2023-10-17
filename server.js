dd653209dc507e69afc45d82ffb58c15eae19385
Sahil Makadia <makadiasahil9846@gmail.com>
2023-10-17 13:59:41 +0530

    Route Setup

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send("Api is running...."))
 
+// Defining routes
+app.use('/api/users', require('./routes/api/users.js'))
+app.use('/api/auth', require('./routes/api/auth.js'))
+app.use('/api/posts', require('./routes/api/posts.js'))
+app.use('/api/profile', require('./routes/api/profile.js'))
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started On Port ${PORT}.`))
