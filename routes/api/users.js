d2a5316a4d31195a0419b3590af033ab99c8cc9d
Sahil Makadia <makadiasahil9846@gmail.com>
2023-10-17 15:21:53 +0530

    Register User Route With Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,28 @@
 const express = require("express")
 const router = express.Router();
+const { check, validationResult } = require("express-validator")
 
 
-// @route GET api/users
-// @desc Test Route
+// @route POST api/users
+// @desc Register User Route
 // @access Public
-router.get('/', (req, res) => res.send("User route"))
+router.post('/', [
+    check("name", "Name is required!").not().isEmpty(),
+    check("email", "Email is required!").not().isEmpty(),
+    check("email", "Please enter a valid email!").isEmail(),
+    check("password", "Password is required!").not().isEmpty(),
+    check("password", "Password must have 6 or more characters!").isLength({ min: 6 })
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }else{
+        res.send(req.body)
+    }
+
+    res.send("User Route")
+})
 
 
 module.exports = router;
