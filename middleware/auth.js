e36a56bfc13b94c07bbd4c51f2ceaa752b0ace49
Sahil Makadia <makadiasahil9846@gmail.com>
2023-10-18 11:20:07 +0530

    Auth Middleware and Auth Route Implemented For Authorized Users

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+    // get token from header
+    const token = req.header('x-auth-token');
+
+    //check if token is not existing
+    if (!token) {
+        return res.status(401).json({ msg: "No token found! Authorization denied." })
+    }
+
+    //check if token is valid or not
+    try {
+        const decoded = jwt.verify(token, config.get("jwtToken"));
+        req.user = decoded.user;
+        next()
+    } catch (error) {
+        res.status(401).json({ msg: "Token is not valid." })
+    }
+}
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const router = express.Router();
-
+const auth = require("../../middleware/auth.js")
+const User = require("../../models/Users.js")
 
 // @route GET api/auth
 // @desc Test Route
 // @access Public
-router.get('/', (req, res) => res.send("Auth route"))
-
+router.get('/', auth, async (req, res) => {
+    try {
+        const fetchedUser = await User.findById(req.user.id).select("-password")
+        res.json(fetchedUser);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error!")
+    }
+})
 
 module.exports = router;
