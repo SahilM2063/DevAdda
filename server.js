722fcb839e355bb70dbbdc18f21f99b6ca5fc63e
Sahil Makadia <makadiasahil9846@gmail.com>
2023-11-22 16:56:20 +0530

    Significant changes in Deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const connectDB = require("./config/db.js")
+const path = require("path")
 
 const app = express();
 
@@ -9,13 +10,22 @@ connectDB();
 // init middleware
 app.use(express.json({ extended: false }))
 
-// app.get('/', (req, res) => res.send("Api is running...."))
 
 // Defining routes
 app.use('/api/users', require('./routes/api/users.js'))
 app.use('/api/auth', require('./routes/api/auth.js'))
 app.use('/api/posts', require('./routes/api/posts.js'))
 app.use('/api/profile', require('./routes/api/profile.js'))
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/dist'))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
