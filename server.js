5b0ce968ac4fa6d2fbd8d675d5f9283d516ab7f0
Sahil Makadia <110756286+SahilM2063@users.noreply.github.com>
2024-01-04 19:21:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,4 +28,4 @@ app.get("*", function (_, res) {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started On Port ${PORT}.`))
+app.listen(PORT, () => console.log(`Server Started On Port No ${PORT}.`))
